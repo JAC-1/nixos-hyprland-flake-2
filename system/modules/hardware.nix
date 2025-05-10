a4ac321e390a7041c29567a771693369fe530d4f
JAC-1 <79427501+JAC-1@users.noreply.github.com>
2025-05-10 20:48:51 +0900

    Add nvidia drivers

--- system/modules/hardware.nix
+++ system/modules/hardware.nix
@@ -4,5 +4,15 @@
  hardware = {
     bluetooth.enable = true;
 
+ # Nvidia?
+#   hardware.nvidia = {
+#    modesetting.enable = true;
+#    powerManagement.enable = false;
+#    powerManagement.finegrained = false;
+#    open = false;
+#    nvidiaSettings = true;
+#    package = config.boot.kernelPackages.nvidiaPackages.stable;
+#   }
+
  };
 }
